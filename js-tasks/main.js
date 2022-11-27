beaec112a2f2f5a9aeaadfc4fc0ea794141b7690
Dmitry Shurshilin <dmitry.shurshilin@gmail.com>
2022-11-27 12:23:11 +0600

    Update the latest task - Add JSDoc and task text

--- js-tasks/main.js
+++ js-tasks/main.js
@@ -92,7 +92,13 @@ const createPhoneNumber = (numbers) => {
     return numbers.reduce((format, number) => format.replace('x', number), format)
 }
 
-// https://www.codewars.com/kata/5277c8a221e209d3f6000b56/train/javascript
+/**
+ * Write a function that takes a string of braces, and determines if the order of the braces is valid.
+ * It should return true if the string is valid, and false if it's invalid.
+ * https://www.codewars.com/kata/5277c8a221e209d3f6000b56/train/javascript
+ * @param {string} braces - A string of braces
+ * @returns {boolean} A braces string valid validity
+ */
 const validBraces = (braces) => {
     const bracesMap = new Map([
         ['(',')'],
